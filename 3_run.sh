882b2641338b94659169466f72c6f54afb2a4c05
YONAMINE Ryo <yonamine@ccw03.cc.kek.jp>
2021-04-26 12:36:11 +0900

    update

--- 3_run.sh
+++ 3_run.sh
@@ -6,16 +6,16 @@
 hostname=`hostname`
 if [[ $hostname =~ cc.kek.jp ]] ;
 then
-bsub -q s -o submit_delphes_E250.Pe2e2h.eL.pR.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.Pe2e2h.eL.pR.tcl delphes_E250.Pe2e2h.eL.pR E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eL.pR.I106479.001.stdhep
+bsub -q s -o submit_delphes_E250.Pe2e2h.eL.pR.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.Pe2e2h.eL.pR E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eL.pR.I106479.001.stdhep
 sleep 1
-bsub -q s -o submit_delphes_E250.Pe2e2h.eR.pL.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.Pe2e2h.eR.pL.tcl delphes_E250.Pe2e2h.eR.pL E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eR.pL.I106480.001.stdhep
+bsub -q s -o submit_delphes_E250.Pe2e2h.eR.pL.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.Pe2e2h.eR.pL E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eR.pL.I106480.001.stdhep
 sleep 1
-bsub -q s -o submit_delphes_E250.P4f_zz_sl.eL.pR.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.P4f_zz_sl.eL.pR.tcl delphes_E250.P4f_zz_sl.eL.pR E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eL.pR.I106575.001.stdhep
+bsub -q s -o submit_delphes_E250.P4f_zz_sl.eL.pR.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.P4f_zz_sl.eL.pR E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eL.pR.I106575.001.stdhep
 sleep 1
-bsub -q s -o submit_delphes_E250.P4f_zz_sl.eR.pL.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.P4f_zz_sl.eR.pL.tcl delphes_E250.P4f_zz_sl.eR.pL E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eR.pL.I106576.001.stdhep
+bsub -q s -o submit_delphes_E250.P4f_zz_sl.eR.pL.log LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.P4f_zz_sl.eR.pL E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eR.pL.I106576.001.stdhep
 else
-LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.Pe2e2h.eL.pR.tcl delphes_E250.Pe2e2h.eL.pR E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eL.pR.I106479.001.stdhep
-LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.Pe2e2h.eR.pL.tcl delphes_E250.Pe2e2h.eR.pL E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eR.pL.I106480.001.stdhep
-LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.P4f_zz_sl.eL.pR.tcl delphes_E250.P4f_zz_sl.eL.pR E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eL.pR.I106575.001.stdhep
-LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen_E250.P4f_zz_sl.eR.pL.tcl delphes_E250.P4f_zz_sl.eR.pL E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eR.pL.I106576.001.stdhep
+LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.Pe2e2h.eL.pR E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eL.pR.I106479.001.stdhep
+LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.Pe2e2h.eR.pL E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eR.pL.I106480.001.stdhep
+LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.P4f_zz_sl.eL.pR E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eL.pR.I106575.001.stdhep
+LCIO/examples/cpp/delphes2lcio/bin/DelphesSTDHEP2LCIO delphes/cards/delphes_card_ILCgen.tcl delphes_E250.P4f_zz_sl.eR.pL E250-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eR.pL.I106576.001.stdhep
 fi
